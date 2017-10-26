910f6ab454cc998877df853d80f3cd0d089d080b
Leo Duffy <leoduffy@hotmail.com>
2017-10-26 13:44:25 +0100

    Add topics route

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
 
 const articlesRouter = require('./articles');
+const topicsRouter = require('./topics');
 
 router.use('/articles', articlesRouter);
+
+router.use('/topics', topicsRouter);
 
 module.exports = router;
