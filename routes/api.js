fbed49048b631253794660d5472ace505230afb5
Leo Duffy <leoduffy@hotmail.com>
2017-10-26 14:50:53 +0100

    [Comments] Update vote count on comment route

--- routes/api.js
+++ routes/api.js
@@ -3,8 +3,15 @@ const router = require('express').Router();
 const articlesRouter = require('./articles');
 const topicsRouter = require('./topics');
 
+const {
+    putCommentVoteCount
+} = require('../controllers/controllers');
+
 router.use('/articles', articlesRouter);
 
 router.use('/topics', topicsRouter);
+
+router.route('/comments/:comment_id')
+    .put(putCommentVoteCount);
 
 module.exports = router;
