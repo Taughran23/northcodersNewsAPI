48bbbc2c4bdcff7bc5b6f8fbbd69d31afce657b5
Leo Duffy <leoduffy@hotmail.com>
2017-10-26 14:51:16 +0100

    [comments] Add tests for putCommentVoteCount

--- spec/api.spec.js
+++ spec/api.spec.js
@@ -172,4 +172,15 @@ describe('API', function () {
         });
     });
   });
+  describe("PUT /api/comments/:comment_id", () => {
+    it("should update the vote count of a particular comment", done => {
+      request(server)
+        .put(`/api/comments/${usefulData.comments[0]._id}?vote=up`)
+        .end((err, res) => {
+          expect(res.status).to.equal(200);
+          expect(res.body.message).to.equal(1);
+          done();
+        });
+    });
+  });
 });
