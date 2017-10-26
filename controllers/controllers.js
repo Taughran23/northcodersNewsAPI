24e7c4e67072f2592a67a62b64b633e725b10d66
Leo Duffy <leoduffy@hotmail.com>
2017-10-26 13:43:35 +0100

    [Topics] Get all topics

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,4 +1,4 @@
-const { Articles } = require("../models/models");
+const { Articles, Topics } = require("../models/models");
 
 exports.getAllArticles = (req, res, next) => {
   Articles.find({}, (err, articles) => {
@@ -6,5 +6,14 @@ exports.getAllArticles = (req, res, next) => {
       return next(err);
     }
     res.status(200).json({ articles: articles });
+  });
+};
+
+exports.getAllTopics = (req, res, next) => {
+  Topics.find({}, (err, topics) => {
+    if (err) {
+      return next(err);
+    }
+    res.status(200).json({ topics: topics });
   });
 };
