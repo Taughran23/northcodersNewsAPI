826bdf50ac1e5a99de42d0deee5e3c365bb8507d
Leo Duffy <leoduffy@hotmail.com>
2017-10-26 14:27:27 +0100

    Article comments route

--- routes/articles.js
+++ routes/articles.js
@@ -1,10 +1,14 @@
 const router = require('express').Router();
 
 const {
-  getAllArticles
+  getAllArticles, 
+   getAllCommentsForArticle
 } = require('../controllers/controllers');
 
 router.route('/')
     .get(getAllArticles);
+
+router.route('/:article_id/comments')
+    .get(getAllCommentsForArticle);
 
 module.exports = router;
